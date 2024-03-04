c2e254e50458d1ca75959d995c21b68d279758b7
ねこ 白い <shiroineko.tfs@gmail.com>
2024-03-04 14:45:03 +0700

    Update build_exe.py

--- meson-1.3.2/build_exe.py
+++ meson-1.3.2/build_exe.py
@@ -1,8 +1,19 @@
 import PyInstaller.__main__
+import pkg_resources
+
+# Get a list of all installed packages
+installed_packages = [pkg.key for pkg in pkg_resources.working_set]
+
+# Construct the list of hidden imports
+hidden_imports = []
+for pkg_name in installed_packages:
+    hidden_imports.extend(['--hidden-import', pkg_name])
 
 PyInstaller.__main__.run([
-    'meson.py',  # Replace 'your_script.py' with the name of your main Python script
+    'meson.py',        # Replace 'meson.py' with the name of your main Python script
     '--onefile',       # This option creates a single executable file
-    '--name', 'meson.exe',  # Specify the name for your executable
+    '--name=meson',    # Specify the name for your executable (without the extension)
     '--clean',         # Cleans temporary files
+    '--collect-all',   # Collect all dependencies
+    *hidden_imports   # Include all installed packages as hidden imports
 ])
